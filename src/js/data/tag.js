dc5da1dc2b4412ef53c236588fd9a1e92fcae40f
crowwan <crowwan@naver.com>
2023-01-09 13:06:27 +0900

    tag data

---
+++ src/js/data/tag.js
@@ -0,0 +1,30 @@
+export const tag = [
+  {
+    name: "html",
+    url: "./asset/html-5.png",
+  },
+  {
+    name: "css",
+    url: "./asset/css-3.png",
+  },
+  {
+    name: "javascript",
+    url: "./asset/js.png",
+  },
+  {
+    name: "node.js",
+    url: "./asset/nodejs.png",
+  },
+  {
+    name: "react.js",
+    url: "./asset/reactjs.png",
+  },
+  {
+    name: "linux",
+    url: "./asset/linux.png",
+  },
+  {
+    name: "git",
+    url: "./asset/git.png",
+  },
+];
