0078461e7ff53b21ebb1e937826587cb5e424438
crowwan <crowwan@naver.com>
2023-01-23 19:42:34 +0900

    modal 통합으로 인한 코드 수정

--- src/js/components/DiscussionItem.js
+++ src/js/components/DiscussionItem.js
@@ -1,17 +1,73 @@
 import { $c } from "../utils/createElement.js";
-import DiscussionModal from "./ui/DiscusionModal.js";
 import toStringByFormatting from "../utils/toStringFormat.js";
+import DiscussionModalInfo from "./DiscussionModalInfo.js";
+import Modal from "./ui/Modal.js";
+import { discussion } from "../discussion/discussions.js";
+import { user } from "../user/user.js";
+import { $ } from "../utils/query.js";
+
 export default function DiscussionItem($app, props) {
   const $li = $c("li");
   $li.className = "discussions";
   $li.dataset.discussionid = props.id;
+  const curDiscussion = discussion.getDiscussions()[props.id];
 
+  const answerHandler = () => {
+    console.log("answered");
+    const $answer = $(".answer-content");
+    const curDiscussion = discussion.getDiscussions()[props.id];
+    const createdAt = new Date().toISOString();
+    curDiscussion.answer = {
+      author: user.getCurrentUser(),
+      bodyHTML: $answer.value,
+      createdAt,
+      id: Date.now(),
+    };
+    discussion.setDiscussion(props.id, curDiscussion);
+  };
+
+  const getBtns = () =>
+    user.getCurrentUser()
+      ? curDiscussion.author === user.getCurrentUser()
+        ? [{ type: "btn-remove", text: "REMOVE" }]
+        : !curDiscussion.answer
+        ? [{ type: "answer", text: "ANSWER" }]
+        : []
+      : [];
+
+  const contentBody = [
+    { text: "TITLE", type: "div", klass: "", content: curDiscussion.title },
+    {
+      text: "CONTENT",
+      type: "div",
+      klass: "",
+      content: curDiscussion.bodyHTML,
+    },
+    curDiscussion.answer && {
+      text: "ANSWER",
+      type: "div",
+      klass: "",
+      content: curDiscussion.answer.bodyHTML,
+    },
+  ];
   $li.addEventListener("click", () => {
-    new DiscussionModal($app, {
-      discussionId: $li.dataset.discussionid,
-      hide: false,
-      answering: false,
-    });
+    new Modal(
+      $app,
+      {
+        discussionId: $li.dataset.discussionid,
+        hide: false,
+        answering: false,
+        body: {
+          title: "DISCUSSION",
+          beforeContent: DiscussionModalInfo(
+            discussion.getDiscussions()[$li.dataset.discussionid]
+          ),
+          contentBody,
+        },
+        btns: getBtns(),
+      },
+      answerHandler
+    );
   });
 
   const tmpl = `
