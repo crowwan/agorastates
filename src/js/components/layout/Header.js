e6c580ec3b15813d713e2d63eff7c98350047384
crowwan <crowwan@naver.com>
2023-01-23 19:41:42 +0900

    modal 통합으로 인한 코드 수정

--- src/js/components/layout/Header.js
+++ src/js/components/layout/Header.js
@@ -1,8 +1,8 @@
 import { user } from "../../user/user.js";
 import { userAPI } from "../../user/userAPI.js";
 import { $c } from "../../utils/createElement.js";
-import SignInModal from "../ui/SignInModal.js";
-import SignUpModal from "../ui/SignUpModal.js";
+import { $ } from "../../utils/query.js";
+import Modal from "../ui/Modal.js";
 
 export default function Header($app, initialState) {
   this.state = initialState;
@@ -15,18 +15,79 @@ export default function Header($app, initialState) {
     this.state = { ...this.state, ...newState };
     this.render();
   };
+  const contentMap = {
+    "SIGN UP": [
+      {
+        text: "id",
+        type: "input",
+        klass: "id",
+      },
+      {
+        text: "password",
+        type: "input",
+        klass: "pw",
+      },
+      {
+        text: "password",
+        type: "input",
+        klass: "url",
+      },
+    ],
+    "SIGN IN": [
+      {
+        text: "id",
+        type: "input",
+        klass: "signInId",
+      },
+      {
+        text: "password",
+        type: "input",
+        klass: "signInPw",
+      },
+    ],
+  };
+  const handlerMap = {
+    "SIGN IN": signInHandle,
+    "SIGN UP": signUpHandle,
+  };
+
+  function signInHandle() {
+    const $id = $(".modal__body.signInId");
+    const $pw = $(".modal__body.signInPw");
+    userAPI.signIn($id.value, $pw.value);
+  }
+  function signUpHandle() {
+    const $id = $(".modal__body.id");
+    const $pw = $(".modal__body.pw");
+    const $url = $(".modal__body.url");
+    userAPI.signUp($id.value, $pw.value, $url.value);
+  }
 
   this.$target.addEventListener("click", (e) => {
     if (e.target.textContent === "LOG OUT") {
-      userAPI.logOut(e.current$target);
+      userAPI.logOut();
     }
-    if (e.target.textContent === "SIGN UP") {
-      new SignUpModal($app, {
-        hide: false,
-      });
-    }
-    if (e.target.textContent === "SIGN IN") {
-      new SignInModal($app, {});
+    if (
+      e.target.textContent === "SIGN UP" ||
+      e.target.textContent === "SIGN IN"
+    ) {
+      new Modal(
+        $app,
+        {
+          hide: false,
+          body: {
+            title: e.target.textContent,
+            contentBody: contentMap[e.target.textContent],
+          },
+          btns: [
+            {
+              type: "",
+              text: "SUBMIT",
+            },
+          ],
+        },
+        handlerMap[e.target.textContent]
+      );
     }
   });
 
