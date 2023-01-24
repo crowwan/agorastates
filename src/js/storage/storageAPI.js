7f24501cf2abc99f47892644c0fbe99aec9d231b
crowwan <crowwan@naver.com>
2023-01-24 10:14:20 +0900

    코드 수정: 로직은 변경안됨

--- src/js/storage/storageAPI.js
+++ src/js/storage/storageAPI.js
@@ -1,23 +1,20 @@
-import { map } from "../utils/functional.js";
 import storage from "./storage.js";
 import { agoraStatesDiscussions } from "../data/data.js";
 
 export const storageAPI = {
   setInitialStorage() {
     const storageList = storage.getStorageList();
-    map(
-      (a) => (!storage.getData(a) ? storage.setData(a, {}) : ""),
-      storageList
-    );
-
     const dObj = {};
+
+    storageList.forEach((a) => {
+      !storage.getData(a) && storage.setData(a, {});
+    });
+
     agoraStatesDiscussions.forEach((e) => {
       dObj[e.id] = e;
     });
 
-    if (Object.keys(storage.getData("discussion")).length === 0) {
-      console.log(storage.getData("discussion"));
+    !Object.keys(storage.getData("discussion")).length &&
       storage.setData("discussion", dObj);
-    }
   },
 };
